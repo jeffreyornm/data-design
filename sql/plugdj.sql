15fcc7d19a49df36d32c51335aabc21df4811594
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-05 15:42:09 -0600

    created table profile, room and subscribe

--- sql/plugdj.sql
+++ sql/plugdj.sql
@@ -1,9 +1,15 @@
 <!-- Drop Table -->
 
-DROP TABLE IF EXISTS `like`;
-DROP TABLE IF EXISTS tweet;
 DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS subscribe;
 
 <!-- Create Table -->
 
+CREATE TABLE profile
 
+
+CREATE TABLE room
+
+
+CREATE TABLE subscribe
