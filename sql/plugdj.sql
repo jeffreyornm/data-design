110ec721db0197359011a85ee9cc7aabb53c9c32
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-05 15:53:37 -0600

    adding into profile table

--- sql/plugdj.sql
+++ sql/plugdj.sql
@@ -7,7 +7,14 @@ DROP TABLE IF EXISTS subscribe;
 <!-- Create Table -->
 
 CREATE TABLE profile
-
+  profileId BINARY(16) NOT NULL,
+  profileActivationToken CHAR(32),
+  profileAvatar
+  profileEmail VARCHAR(128) NOT NULL,
+  profileHash CHAR(97) NOT NULL,
+  profileMusic
+  profileUserName
+  PRIMARY KEY (profileId)
 
 CREATE TABLE room
 
