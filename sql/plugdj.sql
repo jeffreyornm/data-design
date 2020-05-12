d878205cc194ce56fcf314a9839525254f94aa2b
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-12 15:49:54 -0600

    added insert statement model to work off of

--- sql/plugdj.sql
+++ sql/plugdj.sql
@@ -38,3 +38,6 @@ CREATE TABLE subscribe  (
   FOREIGN KEY (subscribeProfileId) REFERENCES profile(profileId),
   FOREIGN KEY (subscribeRoomId) REFERENCES room(roomId)
 );
+
+ INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+ VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway');
